e46bdf90fa7bd50bc74a5da28155f7153e4de7ab
TalMal-ContactMe <mailrelay999@gmail.com>
2022-12-19 15:14:28 +0200

    Initial commit

---
+++ MavenParent/src/main/java/talmal/contact/MavenParent/App.java
@@ -0,0 +1,13 @@
+package talmal.contact.MavenParent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
